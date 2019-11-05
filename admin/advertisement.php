ccb30eb83973d56bd9836a69f3835f64248a2d91
Adilah Suliman <56313878+meAdilah@users.noreply.github.com>
2019-11-06 00:15:23 +0800

    Reduce coding and title changing
    Eliminate unnecessary coding and change the interface title

--- admin/advertisement.php
+++ admin/advertisement.php
@@ -9,14 +9,12 @@ if(!$_SESSION['login']){
 <html>
 <title>MEMBERS PAGE</title>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="w3css.css">
-<link rel="stylesheet" type="text/css" href="w3css1.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+	body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 </style>
 <body class="w3-light-grey w3-content" style="max-width:1600px">
 
@@ -40,7 +38,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 ?>
   <div class="content" style="padding:16px;">
 		<fieldset style=" background-color:white;">
-			<legend style="font-size:25px; color:#e69900;"><b>E-Advertisement</b></legend>
+			<legend style="font-size:25px; color:#e69900;"><b>Club Advertisement</b></legend>
 				<table cellpadding="5" margin="10" >
 				<tr>
 				<?php
@@ -58,14 +56,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 				
 				</table>
 		</fieldset>
-  
   </div>
-  
-
-  
-
- 
-
+	
 <script>
 // Script to open and close sidebar
 function w3_open() {
