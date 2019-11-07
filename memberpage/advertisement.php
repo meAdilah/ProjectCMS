e81014b23b58756a3f36a56d45709e65dadcbe39
Adilah Suliman <56313878+meAdilah@users.noreply.github.com>
2019-11-07 22:02:27 +0800

    reduce line
    reduce the unnecessary line

--- memberpage/advertisement.php
+++ memberpage/advertisement.php
@@ -9,9 +9,7 @@ if(!$_SESSION['login']){
 <html>
 <title>MEMBERS PAGE</title>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="w3css.css">
-<link rel="stylesheet" type="text/css" href="w3css1.css">
+
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -60,11 +58,6 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 		</fieldset>
   
   </div>
-  
-
-  
-
- 
 
 <script>
 // Script to open and close sidebar
